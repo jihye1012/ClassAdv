d837f863429bdee00879edf506a32849590b5fa4
Jihye Yang <s2127@e-mirim.hs.kr>
2023-05-25 15:03:44 +0900

    속성 실습

---
+++ ClassAdv/Box.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Runtime.Remoting.Messaging;
+
+namespace ClassAdv
+{
+    internal class Box
+    {
+        private int width;
+        private int Width
+        {
+            get { return width; }
+            set {
+                if(value <= 0)
+                {
+                    throw (new Exception("width는 양수여야함"));
+                }
+                width = value;
+            }
+        }
+        private int height;
+        private int Height
+        {
+            get { return height; }
+            set
+            {
+                if (value <= 0)
+                {
+                    throw (new Exception("height는 양수여야함"));
+                }
+                height = value;
+            }
+        }
+
+        public Box(int width, int height)
+        {
+            this.Width = width;
+            this.Height = height;
+        }
+    }
+}
